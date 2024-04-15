c0da7a9508ba24e534d79af3e232ef01ebf0b735
Rymouu <151671598+Rymouu@users.noreply.github.com>
2024-04-15 12:11:45 +0200

    Update ServerMain.java

--- server/ServerMain.java
+++ server/ServerMain.java
@@ -1,7 +1,8 @@
-package serveur;
-
-class ServerMain {
+package server;
+// Main.java (dans le package server)
+public class Main {
     public static void main(String[] args) {
-        server server = new Server(); 
+        Server server = new Server();
+        server.run(); // Appel de la méthode main de la classe Server pour lancer le serveur
     }
 }
